8a8fa24ce8c16ba9fef0b2ee8d0d2b9e50a2368a
Kevin Pennekamp <github@crinkle.dev>
2021-07-03 20:11:20 +0200

    build fixes

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-// import adapter from '@sveltejs/adapter-netlify';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
+// import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
