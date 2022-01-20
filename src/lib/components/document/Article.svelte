5d685eaf623f560dca9339bcd95b13e7135fd8d1
Kevin Pennekamp <github@crinkle.dev>
2022-01-20 19:46:02 +0100

    feat: updated styling package

--- src/lib/components/document/Article.svelte
+++ src/lib/components/document/Article.svelte
@@ -2,6 +2,6 @@
 	export let doc;
 </script>
 
-<article class="border-hover border-w-1 bg-back-secondary flow-y flow-g-1 radius-00 py-2 mb-1">
+<article class="border-hover border-w-1 bg-back-secondary stack stack-g-1 radius-00 py-2 mb-1 lh-2">
 	{@html doc.html}
 </article>
